2fe552e0c4881fb332518d529b05f7082d4b4c01
Garrett Halstein <garrett.halstein@emory.edu>
2019-07-22 11:29:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
 
   const form = document.querySelector('#joke-form');
   const jokeList = document.querySelector('#joke-list');
-  const newJokeLi = document.createElement('li');
   const input = document.querySelector('#name-input');
-  // let joke = "";
 
-  function fetchJoke(username){
+  function fetchJoke(username, newJokeLi){
     fetch('https://icanhazdadjoke.com/', {
       headers: {
         "Accept": "application/json"
       }
     })
     .then(res => res.json())
-    .then(data => newJokeParser(username, data.joke));
+    .then(data => newJokeParser(username, data.joke, newJokeLi));
     // .then(jokeData => console.log(jokeData));
   }
 
-  function newJokeParser(username, joke) {
-    newJokeLi.innerHTML = `
-    <span class="username">${username} says: ${joke}</span>
-    `
+  function newJokeParser(username, joke, newJokeLi) {
+    newJokeLi.innerHTML += ` <span class="username">${username} says: </span>${joke}`;
   }
 
   form.addEventListener('submit', (event) => {
     event.preventDefault();
     const username = input.value;
     if(username === "") return;
-    fetchJoke(username);
-    jokeList.appendChild(newJokeLi);
+    let newJokeLi = document.createElement('li');
+    fetchJoke(username, newJokeLi);
+    jokeList.append(newJokeLi);
   });
